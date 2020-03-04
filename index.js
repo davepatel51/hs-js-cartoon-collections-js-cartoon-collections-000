3f48b036b45c431cca0fe31eceee09086ed55c37
Dave Patel <ddp2134@gmail.com>
2020-03-04 03:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
 }
+
+function summonCaptainPlanet(planeteerCalls){
+return planeteerCalls.map(x=>x.toUpperCase()+"!")
+}
+
+function longPlaneteerCalls(wordsize){
+  for(var x=0;x<wordsize.length;x++){
+    if (wordsize[x].length>4){
+      return true
+    }
+  }
+  return false
+}
+
+
+
+
+function dwarfRollCall(dwarves) {
+  var DRF= [];
+  dwarves.forEach(function(name, x) {
+  	DRF += (x+1 + ". " + name+ " ");
+  });
+  return DRF;
+}
+
+function findTheCheese (cheese) {
+  for(var x=0;x<cheese.length;x++){
+  if (cheese[x]=="cheddar"
+  ||cheese[x]=="gouda"
+  ||cheese[x]=="camembert"
+  ){
+    return cheese[x]
+  }
+}
+return "no cheese!"
+}
+
+  
